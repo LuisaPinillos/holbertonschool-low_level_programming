2258808c47677fa2df69f1c71cdab13b5bbf904d
LuisaPinillos <3389@holbertonschool.com>
2021-06-20 16:19:44 -0700

    prints Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -12,8 +12,8 @@
 
  */
 
-int main(void)  
+int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle"); 
-  return (0);  
+puts("\"Programming is like building a multilingual puzzle"); 
+return (0);  
 }
