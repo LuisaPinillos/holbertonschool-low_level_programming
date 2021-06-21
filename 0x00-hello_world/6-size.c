ec24151bd207ddcc0bf284d2503c714466d9366b
LuisaPinillos <3389@holbertonschool.com>
2021-06-21 09:08:13 -0700

    bytes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,9 +14,9 @@ int b;
 long int c;
 long long d;
 float e;
-printf("size of char: %d byte(s)\n", size of(a));
-printf("size of int:  %d byte(s)\n", size of(b));
-printf("size of long int: %d byte(s)\n", size of(c));
-printf("size of long long: %d byte(s)\n", size of(d));
-printf("size of float: %d byte(s)\n", size of(e));
+printf("size of char: %lu byte(s)\n", size of(a));
+printf("size of int:  %lu byte(s)\n", size of(b));
+printf("size of long int: %lu byte(s)\n", size of(c));
+printf("size of long long: %lu byte(s)\n", size of(d));
+printf("size of float: %lu byte(s)\n", size of(e));
 }
