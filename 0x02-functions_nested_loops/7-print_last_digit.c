f9350dd9bfa95c811a32f53fee2e74e04fd8e4ee
LuisaPinillos <3389@holbertonschool.com>
2021-06-25 09:37:49 -0700

    try

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,14 +13,16 @@ int print_last_digit(int n)
 	if (n > 0)
 	{
 		_putchar((n % 10) + '0');
+		return ((n % 10) + '0');
 	}
 	else if (n < 0)
 	{
-		_putchar((mul % 10) + '0');
+		_putchar((n % 10) + '0');
+		return ((mul % 10) + '0');
 	}
 	else
 	{
 		_putchar('0');
+		return ('0');
 	}
-	return (0);
 }
