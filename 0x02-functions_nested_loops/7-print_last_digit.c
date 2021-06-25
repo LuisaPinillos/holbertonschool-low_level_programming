22bfab0fdcf3173edb0a0329bf2e645a97c9d27e
LuisaPinillos <3389@holbertonschool.com>
2021-06-25 09:25:11 -0700

    prints the last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - omputes the absolute value of an integer
+ * @n: The character in ASCII
+ * Return: 1 greter than 0. 0 otherwise
+ */
+
+int print_last_digit(int n)
+{
+	int mul = n * -1;
+
+	if (n > 0)
+	{
+		_putchar((n % 10) + '0');
+	}
+	else if (n < 0)
+	{
+		_putchar((mul % 10) + '0');
+	}
+	else
+	{
+		_putchar('0');
+	}
+	return (0);
+}
