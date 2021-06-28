6a0bb98e1329dce7b4cc7ce680c0df6df08dbb60
LuisaPinillos <3389@holbertonschool.com>
2021-06-27 19:37:17 -0700

    prints the 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ * times_table - omputes the absolute value of an integer
+ *
+ * Return: nothing
+ */
+
+void times_table(void)
+{
+	int f, n;
+	int mul;
+
+	for (f = 0; f <= 9; f++)
+	{
+		for (n = 0; n <= 9; n++)
+		{
+			mul = f * n;
+
+			if (mul > 9)
+			{
+				_putchar((mul / 10) + '0');
+				_putchar((mul % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+
+			else
+			{
+				_putchar(mul + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+
+		}
+		_putchar('\n');
+	}
+}
