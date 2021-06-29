0cd925f9bb1af3a1e51ee2dbd86304080fdcac8d
LuisaPinillos <3389@holbertonschool.com>
2021-06-29 15:02:11 -0700

    prints a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_rev - prints a string
+ *@s: string
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int len;
+	int i;
+
+	len = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len++;
+	}
+	for (i = len; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
