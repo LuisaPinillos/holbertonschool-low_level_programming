b0200a31d4de4ff3e4cc54b01b05905876851d0e
LuisaPinillos <3389@holbertonschool.com>
2021-06-28 21:37:36 -0700

    swaps the values of two integers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,16 +1,18 @@
 #include "holberton.h"
 
 /**
- * swap_int - omputes the absolute value of an integ
- *
+ * swap_int - swaps the values of a to b
+ *@a: value
+ *@b: value
  * Return: 0
  */
 
 void swap_int(int *a, int *b)
 {
 	int p;
+
 	p = *a;
 	*a = *b;
 	*b = p;
-	
+
 }
