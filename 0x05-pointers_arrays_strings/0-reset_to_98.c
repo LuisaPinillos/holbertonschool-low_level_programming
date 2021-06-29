f55f27371bade02476653fa1482ca58ce5876057
LuisaPinillos <3389@holbertonschool.com>
2021-06-28 21:21:57 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - omputes the absolute value of an integ
+ *
+ *Return: 0
+ */
+
+void reset_to_98(int *n)
+{	
+	*n = 98;
+	
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * swap_int - omputes the absolute value of an integ
+ *
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int p;
+	p = *a;
+	*a = *b;
+	*b = p;
+	
+}
