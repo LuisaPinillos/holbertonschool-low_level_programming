e646cd1c7c09590bf42ab6c6439473a6f09fd283
LuisaPinillos <3389@holbertonschool.com>
2021-06-29 15:25:57 -0700

    prints a string, followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _puts - prints a string
+ *@str: string
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
