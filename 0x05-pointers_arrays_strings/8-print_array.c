f39a4513ee7a95618aff4de50e43ca2fef412643
LuisaPinillos <3389@holbertonschool.com>
2021-06-29 21:21:39 -0700

    prints n elements of an array of integers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+
+/**
+ * print_array - prints half of a string
+ *@a: array
+ *@n: positions
+ * Return: 0
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+}
