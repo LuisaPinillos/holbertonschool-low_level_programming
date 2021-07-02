1fab64c861993db15fc2a7f5f19031cbcb258de4
LuisaPinillos <3389@holbertonschool.com>
2021-07-01 20:36:53 -0700

    adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * add - omputes the absolute value of an integer
+ *@n: number
+ *@b: number
+ * Return: nothing
+ */
+
+int add(int b, int n)
+{
+	int sum;
+
+	sum = b + n;
+
+	return (sum);
+}
