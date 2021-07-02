20c6d3f13f061295061082fb8da7ac43baf60461
LuisaPinillos <3389@holbertonschool.com>
2021-07-02 15:46:28 -0700

    concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * *_strcat - concatenates two strings
+ *@dest: array
+ *@src: positions
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j = 0;
+	int len = 0;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		len++;
+	}
+	for (j = 0; src[j] != '\0'; j++, len++)
+	{
+		dest[len] = src[j];
+	}
+	return (dest);
+}	
