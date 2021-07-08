7019e16e3910c623abdd8dfca7ecffde7113c2ff
LuisaPinillos <3389@holbertonschool.com>
2021-07-08 09:05:47 -0700

    prints a string, followed by a new line.

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - prints a string
+ *@s:string
+ * Return: Always 0.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+	else
+		_putchar('\n');
+}
