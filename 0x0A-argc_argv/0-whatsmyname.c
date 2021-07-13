bae2eebbeb82b53195652ec21795d8df61e56a3f
LuisaPinillos <3389@holbertonschool.com>
2021-07-13 16:50:10 -0500

    prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - program that prints its name 
+ * @argc: number of arguments
+ * @argv: matrix of arguments
+ * Return: s
+ */
+
 int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s \n", argv[i]);
-	}
+	printf("%s\n", argv[0]);
+	(void)argc;
 	return (0);
 }
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Points about Argc, Argv
