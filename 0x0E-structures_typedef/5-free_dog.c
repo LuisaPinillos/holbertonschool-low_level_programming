5b8b2a4632c7a0f187f00de47f8c86c3cd2ed8a0
LuisaPinillos <3389@holbertonschool.com>
2021-07-19 14:15:19 -0700

    Function that frees dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "dog.h"
+
+/**
+ * free_dog - Memory free
+ * @d: Pointer
+ * Description:the memory is free
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d);
+}
