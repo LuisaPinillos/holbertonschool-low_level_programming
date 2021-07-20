3d07718c39d994967ffedf5fb4e219c8c9ded695
LuisaPinillos <3389@holbertonschool.com>
2021-07-20 01:41:38 -0700

    function that concatenates two strings with malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+#include "holberton.h"
 
 /**
  * str_concat - function that concatenates two strings
@@ -10,73 +11,39 @@
 
 char *str_concat(char *s1, char *s2)
 {
-    int i, a, result;
-    int suma1 = 0, suma2 = 0;
-    char *s;
+	int long1 = 0;
+	int long2 = 0;
+	int suma, a, b;
+	char *cpstr;
 
-    if (s1 && s2 )
-    {
-        suma1 = strlen(s1);
-        suma2 = strlen(s2);
-        result = suma1 + suma2;
-        s = malloc((sizeof(char) * result) + 1);
-        if (s == 0)
-        {
-        return (NULL);
-        }
-        for (i = 0; i < suma1; i++)
-        {
-        s[i] = s1[i];
-        }
-        for (a = 0; a < suma2; a++, i++)
-        {
-        s[i] = s2[a];
-        }
-        s[i] = '\0';
-        return (s);
-    }
-    else if (s1 == NULL && s2)
-    {
-        suma2 = strlen(s2);
-        s = malloc((sizeof(char) * suma2) + 1);
-        if (s == 0)
-        {
-        return (NULL);
-        }
-        for (i = 0; i < suma1; i++)
-        {
-        s[i] = s1[i];
-        }
-        for (a = 0; a < suma2; a++, i++)
-        {
-        s[i] = s2[a];
-        }
-        s[i] = '\0';
-        return (s);
-    }
-    else if(s1 && s2 == NULL)
-    {
-        suma1 = strlen(s1);
-        s = malloc((sizeof(char) * suma1) + 1);
-        if (s == 0)
-        {
-        return (NULL);
-        }
-        for (i = 0; i < suma1; i++)
-        {
-        s[i] = s1[i];
-        }
-        for (a = 0; a < suma2; a++, i++)
-        {
-        s[i] = s2[a];
-        }
-        s[i] = '\0';
-        return (s);
-    }
-    else 
-    {
-      s = malloc(sizeof(char) * 1);
-      s[0] = '\0';
-      return (s);
-    }
+	if (!s1)
+	{
+		s1 = "";
+	}
+	if (!s2)
+	{
+		s2 = "";
+	}
+
+	long1 = strlen(s1);
+	long2 = strlen(s2);
+	suma = long1 + long2;
+
+	cpstr = malloc((sizeof(char) * suma) + 1);
+
+	if (!cpstr)
+	{
+		return (NULL);
+	}
+	for (a = 0; a < long1; a++)
+	{
+		cpstr[a] = s1[a];
+	}
+	for (b = 0; b < long2; b++)
+	{
+		cpstr[a] = s2[b];
+		a++;
+	}
+	cpstr[a] = '\0';
+	return (cpstr);
 }
