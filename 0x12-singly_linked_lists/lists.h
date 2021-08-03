26288650a51e6de6d9559e95d6fd1904ffab1aa0
LuisaPinillos <3389@holbertonschool.com>
2021-08-03 15:31:28 -0700

    add a function to library

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -30,5 +30,6 @@ size_t list_len(const list_t *h);
 int len(const char *str);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
