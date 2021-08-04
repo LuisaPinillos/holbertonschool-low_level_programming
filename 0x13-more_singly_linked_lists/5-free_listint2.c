e39d560ea8e15e5b457f2b3ba1d00048449d4156
LuisaPinillos <3389@holbertonschool.com>
2021-08-03 21:20:00 -0700

    free a list with head double pointer

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,15 +1,21 @@
 #include "lists.h"
 
-void free_listint2(listint_t **head) {
+/**
+ * free_listint2 - function that frees a listint_t list.
+ * @head: head of the list
+ *
+ * Return: Always 0.
+ */
 
-    listint_t* temp = (*head);
+void free_listint2(listint_t **head)
+{
+	listint_t *temp = (*head);
 
-    while((*head) != NULL){
-
-        (*head) = (*head)->next;
-        free(temp);
-        temp = (*head);
-    }
-
-    *head = NULL;
+	while ((*head) != NULL)
+	{
+		(*head) = (*head)->next;
+		free(temp);
+		temp = (*head);
+	}
+	*head = NULL;
 }
