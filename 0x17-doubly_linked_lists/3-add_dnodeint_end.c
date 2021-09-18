84dbd447da1a71b4feb25ae4c77eb7543b9c7550
LuisaPinillos <luisapinillos20@gmail.com>
2021-09-18 00:24:27 -0500

    Add a new node at the end of a dlistint_t list.

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -31,6 +31,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		temp = temp->next;
 	}
 	temp->next = new_node;
-	new_node->prev = *head;
+	new_node->prev = temp;
 	return (new_node);
 }
